e7f1e86e1a95f13a37fe50284f65c78faef67c59
Hansol YOO <78459995+yhansol145@users.noreply.github.com>
2023-03-03 00:15:23 +0900

    jpa error

--- src/main/java/com/jojoldu/book/springboot/domain/posts/PostsRepository.java
+++ src/main/java/com/jojoldu/book/springboot/domain/posts/PostsRepository.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 public interface PostsRepository extends JpaRepository<Posts, Long> {
 
-    @Query("SELECT p FROM Posts P ORDER BY p.id DESC")
+    @Query("SELECT p FROM Posts p ORDER BY p.id DESC")
     List<Posts> findAllDesc();
 
 }
