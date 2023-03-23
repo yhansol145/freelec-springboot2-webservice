4e1c65d12534817804c8f602a23b41e0c1c06c41
Hansol YOO <78459995+yhansol145@users.noreply.github.com>
2023-03-23 22:02:58 +0900

    게시글 수정기능 추가

--- src/main/resources/static/js/app/index.js
+++ src/main/resources/static/js/app/index.js
@@ -1,12 +1,15 @@
 var main = {
-    init: function(){
+    init: function(){ debugger
         var _this = this;
         $('#btn-save').on('click', function(){
             _this.save();
         });
+        $('#btn-update').on('click', function(){
+            _this.update();
+        })
     },
 
-    save: function(){
+    save: function(){ debugger
         var data = {
             title: $('#title').val(),
             author: $('#author').val(),
@@ -23,6 +26,28 @@ var main = {
             alert('글이 등록되었습니다.');
             window.location.href = '/';
         }).fail(function (error) {
+            alert(JSON.stringify(error));
+        });
+    },
+
+    update: function(){ debugger
+        var data = {
+            title: $('#title').val(),
+            content: $('#content').val()
+        };
+
+        var id = $('#id').val();
+
+        $.ajax({
+            type: 'PUT',
+            url: '/api/v1/posts/'+id,
+            dataType: 'json',
+            contentType: 'application/json; charset=utf-8',
+            data: JSON.stringify(data)
+        }).done(function() {
+            alert("글이 수정되었습니다.");
+            window.location.href = '/';
+        }).fail(function(error){
             alert(JSON.stringify(error));
         });
     }
